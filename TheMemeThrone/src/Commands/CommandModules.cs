9d62a36c96fd8b79fe78a45f52a08652106bb00c
dill0wn <wallsdil@gmail.com>
2021-04-01 11:28:43 -0400

    building up a complex embedded image message with all sorts of fields

--- TheMemeThrone/src/Commands/CommandModules.cs
+++ TheMemeThrone/src/Commands/CommandModules.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Linq;
 using System.Threading.Tasks;
+using Discord;
 using Discord.Commands;
 
 namespace MemeThroneBot.Commands
@@ -47,6 +48,40 @@ namespace MemeThroneBot.Commands
             {
                 await ReplyAsync(caption.Text);
             }
+        }
+
+        [Command("meme")]
+        [Summary("Posts a Meme.")]
+        public async Task GetMemeAsync()
+        {
+            var meme = await context.MemeCards.AsAsyncEnumerable().FirstOrDefaultAsync();
+
+            Console.WriteLine($"get meme {meme}");
+            if (meme == null)
+            {
+                await ReplyAsync("No memes availble");
+                return;
+            }
+
+            var embed = new EmbedBuilder()
+                .WithFields(
+                    new EmbedFieldBuilder { Name = "Option 1️⃣", Value = "Bar1", IsInline = true },
+                    new EmbedFieldBuilder { Name = "Option 2️⃣", Value = "Bar2", IsInline = true },
+                    new EmbedFieldBuilder { Name = "Option 3️⃣", Value = "Bar3", IsInline = false }
+                )
+                .WithTitle("Got Meme")
+                .WithDescription("This is a long message with some rich text.")
+                .WithColor(0x0000ff)
+                .WithImageUrl(meme.Url);
+            var message = await ReplyAsync(embed: embed.Build());
+            await message.AddReactionsAsync(new IEmote[]{
+                new Emoji("1️⃣"),
+                new Emoji("2️⃣"),
+                new Emoji("3️⃣"),
+            });
+
+
+            Console.WriteLine($"Embeded image to {message.Channel.Id}, {message.Author.Id}, {message.GetJumpUrl()}");
         }
     }
 }
